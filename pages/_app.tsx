0b23b383cd34849bcd87acb94bd97907bfc43407
Logan Yu <yu.logan@gmail.com>
2023-02-14 13:26:33 -0500

    add template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,23 +4,15 @@ import { SessionProvider } from 'next-auth/react';
 import {Provider as StyletronProvider} from 'styletron-react';
 import {DarkTheme, BaseProvider, styled} from 'baseui';
 import {styletron} from '../styletron';
-
-const Centered = styled('div', {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100%',
-});
+import '../styles.css'
 
 const App = ({ Component, pageProps }: AppProps<{session: Session;}>) => {
   return (
     <StyletronProvider value={styletron}>
       <BaseProvider theme={DarkTheme}>
-        <Centered>
-          <SessionProvider session={pageProps.session}>
+        <SessionProvider session={pageProps.session}>
             <Component {...pageProps} />
-          </SessionProvider>
-        </Centered>
+        </SessionProvider>
       </BaseProvider>
     </StyletronProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,29 @@ import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
 import prisma from '../lib/prisma';
 
+// components
+import Navigation from "../components/Navigation"
+
+// baseweb
+import { Table } from "baseui/table";
+import {useStyletron} from 'baseui';
+import {Grid, Cell, BEHAVIOR} from 'baseui/layout-grid';
+import {
+  HeaderNavigation,
+  ALIGN,
+  StyledNavigationList,
+  StyledNavigationItem
+} from "baseui/header-navigation";
+import { StyledLink } from "baseui/link";
+import { Button } from "baseui/button";
+import { MessageCard } from "baseui/message-card";
+import {
+  Card,
+  StyledBody,
+  StyledAction
+} from "baseui/card";
+import { Tag } from "baseui/tag";
+
 export const getStaticProps: GetStaticProps = async () => {
   const feed = await prisma.post.findMany({
     where: { published: true },
@@ -22,6 +45,87 @@ export const getStaticProps: GetStaticProps = async () => {
 type Props = {
   feed: PostProps[]
 }
+
+const Exchange: React.FC<Props> = (props) => {
+  return (
+    <div>
+      <Navigation></Navigation>
+      <Grid behavior={BEHAVIOR.fixed}>
+        <Cell span={12}>
+          <Card title="NFTs" overrides={{Root: {style: {marginTop: '10px'}}}}>
+            <StyledBody>
+              Proin ut dui sed metus pharetra hend rerit vel non
+              mi. Nulla ornare faucibus ex, non facilisis nisl.
+              Proin ut dui sed metus pharetra hend rerit vel non
+              mi. Nulla ornare faucibus ex, non facilisis nisl.
+            </StyledBody>
+            <StyledAction>
+              <Tag>this is a tag</Tag>
+            </StyledAction>
+          </Card>
+        </Cell>
+        
+        <Cell span={12}>
+          <Card title="Tech" overrides={{Root: {style: {marginTop: '10px'}}}}>
+            <StyledBody>
+              Proin ut dui sed metus pharetra hend rerit vel non
+              mi. Nulla ornare faucibus ex, non facilisis nisl.
+              Proin ut dui sed metus pharetra hend rerit vel non
+              mi. Nulla ornare faucibus ex, non facilisis nisl.
+            </StyledBody>
+            <StyledAction>
+              <Tag>this is a tag</Tag>
+            </StyledAction>
+          </Card>
+        </Cell>
+        <Cell span={12}>
+          <Card title="Trading" overrides={{Root: {style: {marginTop: '10px'}}}}>
+            <StyledBody>
+              Proin ut dui sed metus pharetra hend rerit vel non
+              mi. Nulla ornare faucibus ex, non facilisis nisl.
+              Proin ut dui sed metus pharetra hend rerit vel non
+              mi. Nulla ornare faucibus ex, non facilisis nisl.
+            </StyledBody>
+            <StyledAction>
+              <Tag>this is a tag</Tag>
+            </StyledAction>
+          </Card>
+        </Cell>
+        <Cell span={12}>
+          <Card title="Tags" overrides={{Root: {style: {marginTop: '10px'}}}}>
+            <StyledBody>
+              Proin ut dui sed metus pharetra hend rerit vel non
+              mi. Nulla ornare faucibus ex, non facilisis nisl.
+              Proin ut dui sed metus pharetra hend rerit vel non
+              mi. Nulla ornare faucibus ex, non facilisis nisl.
+            </StyledBody>
+            <StyledAction>
+              <Tag>this is a tag</Tag>
+            </StyledAction>
+          </Card>
+        </Cell>
+      </Grid>
+    </div>
+  )
+}
+
+const Inner: React.FunctionComponent<{}> = ({children}) => {
+  const [css, theme] = useStyletron();
+  return (
+    <div
+      className={css({
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: theme.colors.accent200,
+        color: theme.colors.accent700,
+        padding: '.25rem',
+      })}
+    >
+      {children}
+    </div>
+  );
+};
 
 const Blog: React.FC<Props> = (props) => {
   return (
@@ -54,4 +158,4 @@ const Blog: React.FC<Props> = (props) => {
   )
 }
 
-export default Blog
+export default Exchange
