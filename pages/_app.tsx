bc00a4a87a030b296cbe292b133dd2bb6e42f4b5
Logan Yu <yu.logan@gmail.com>
2023-02-14 11:50:12 -0500

    fix session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import { AppProps } from "next/app";
+import { Session } from "next-auth";
 import { SessionProvider } from 'next-auth/react';
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }: AppProps<{session: Session;}>) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
