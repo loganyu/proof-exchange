02032e79acc678f0ef93db5960f8576ebd976eed
Logan Yu <yu.logan@gmail.com>
2023-02-14 12:11:47 -0500

    add baseweb

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,28 @@
 import { AppProps } from "next/app";
 import { Session } from "next-auth";
 import { SessionProvider } from 'next-auth/react';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {DarkTheme, BaseProvider, styled} from 'baseui';
+import {styletron} from '../styletron';
+
+const Centered = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+});
 
 const App = ({ Component, pageProps }: AppProps<{session: Session;}>) => {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <StyletronProvider value={styletron}>
+      <BaseProvider theme={DarkTheme}>
+        <Centered>
+          <SessionProvider session={pageProps.session}>
+            <Component {...pageProps} />
+          </SessionProvider>
+        </Centered>
+      </BaseProvider>
+    </StyletronProvider>
   );
 };
 
